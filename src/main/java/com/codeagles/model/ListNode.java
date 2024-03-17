96daa28f5e6349f58af36a47a7b19d93e0fff06d
houchunan <hcn0521@outlook.com>
2024-03-17 21:53:59 +0800

    提交

--- src/main/java/com/codeagles/model/ListNode.java
+++ src/main/java/com/codeagles/model/ListNode.java
@@ -3,8 +3,8 @@ package com.codeagles.model;
 public class ListNode {
       public int val;
       public ListNode next;
-      ListNode() {}
+      public ListNode() {}
       ListNode(int val) { this.val = val; }
-      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+      public ListNode(int val, ListNode next) { this.val = val; this.next = next; }
 
 }
