c9ae800db4739e183e1ea07e7bf72a92ed7ffda2
houchunan <hcn0521@outlook.com>
2024-03-17 14:06:33 +0800

    提交

---
+++ src/main/java/com/codeagles/linklist/LC1290.java
@@ -0,0 +1,26 @@
+package com.codeagles.linklist;
+
+/**
+ * 1290. Convert Binary Number in a Linked List to Integer
+ */
+public class LC1290  {
+
+    public int getDecimalValue(ListNode head) {
+        int res = 0;
+        while(head != null) {
+            res = (res<<1) | head.val ;
+            head = head.next;
+        }
+        return res;
+    }
+
+    public int getDecimalValueV2(ListNode head) {
+        int res = 0;
+        while(head != null) {
+            res = res * 2 + head.val;
+            head = head.next;
+        }
+        return res;
+    }
+
+}
---
+++ src/main/java/com/codeagles/linklist/LC206.java
@@ -0,0 +1,25 @@
+package com.codeagles.linklist;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+public class LC206 {
+    public ListNode reverseList(ListNode head) {
+        ListNode last = null;
+        while(head != null) {
+            ListNode nextHead= head.next;
+            head.next = last;
+            last = head;
+            head = nextHead;
+
+        }
+        return last;
+    }
+}
